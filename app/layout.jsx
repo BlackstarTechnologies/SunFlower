c174cbbb01af3f08be22958055b46d2ca940c82c
iRobot98 <matendejudah@gmail.com>
2023-07-07 09:03:03 +0000

    working on app

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+// import Nav from "@components/Nav";
+// import Provider from "@components/Provider";
+import React from "react";
+
+export const metadata = {
+  title: "SunFlower",
+  description: "MAke your money work for you",
+};
+
+function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
